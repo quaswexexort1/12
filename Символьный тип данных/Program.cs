648004e35aea9215b6e5fabf3687b4f6b22b9a78
SHEL <SHEL@DESKTOP-6SFOS1M>
2024-11-22 12:21:03 +0200

    Добавьте файлы проекта.

---
+++ Символьный тип данных/Program.cs
@@ -0,0 +1,15 @@
+﻿
+        string zashifrovan = "olleH dlroW olleH avcds";
+
+        string razshifrovan = Decrypt(zashifrovan);
+
+        Console.WriteLine($"Зашифрованное сообщение: {zashifrovan}");
+        Console.WriteLine($"Расшифрованное сообщение: {razshifrovan}");
+
+
+
+static string Decrypt(string message)
+{
+    return string.Join(" ", message.Split(' ').Select(word => new string(word.Reverse().ToArray())));
+}
+
